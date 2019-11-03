ea1633a44d29b9fb3c86ea3025a62262896f371d
Kumar Abhirup <kumar@iqubex.com>
2019-11-03 14:25:14 +0530

    👌 IMPROVE: Use a better picture of Kumar Abhirup

--- packages/web/src/api/navBar.js
+++ packages/web/src/api/navBar.js
@@ -1,7 +1,7 @@
 import React from 'react' // For making JSX work 😆
 
 export const navBar = {
-  logo: `/static/prebuilt/images/kumar-abhirup.jpg`,
+  logo: `/static/prebuilt/images/kumar-1024x1024.jpg`,
   menu: [
     { name: 'home', link: `/` },
     { name: 'portfolio', link: `/portfolio` },
