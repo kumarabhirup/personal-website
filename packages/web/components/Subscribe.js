e598fd53735e0e6f1080efdb9f8230e3e47c9fd7
Kumar Abhirup <kumar@iqubex.com>
2020-10-20 22:16:37 +0530

    😅 TRY: Try Vercel

--- packages/web/components/Subscribe.js
+++ packages/web/components/Subscribe.js
@@ -9,46 +9,43 @@ export default function Subscribe() {
   const toSubmitEmail = e => {
     e.preventDefault()
 
-    let email = document.getElementById("paemail").value
+    const email = document.getElementById('paemail').value
 
-    window.open(
-      'https://propagate.at/share/kumar/email/' + email,
-      "_blank"
-    )
+    window.open(`https://propagate.at/share/kumar/email/${email}`, '_blank')
 
     return false
   }
 
   return (
     <>
-    <form
-      style={{
-        textAlign: 'center',
-        height: '100%',
-      }}
-      onSubmit={toSubmitEmail}
-    >
-      <h1 style={{ lineHeight: '50px' }}>
-        <label htmlFor="paemail">Stay Updated</label>
-      </h1>
-      <p className="style">
-        If you like what I write on my blog and twitter, <br />I bet you gonna
-        love the stuff I'll be mailing you!
-        <br />I don't spam, really!
-      </p>
-      <p>
-        <input
-          type="email"
-          style={{ width: '140px' }}
-          name="email"
-          id="paemail"
-          placeholder="Your awesome email address!"
-          required
-        />
-      </p>
-      <input type="hidden" value="1" name="embed" />
-      <input type="submit" value="Welcome to the club 🔥" />
-    </form>
+      <form
+        style={{
+          textAlign: 'center',
+          height: '100%',
+        }}
+        onSubmit={toSubmitEmail}
+      >
+        <h1 style={{ lineHeight: '50px' }}>
+          <label htmlFor="paemail">Stay Updated</label>
+        </h1>
+        <p className="style">
+          If you like what I write on my blog and twitter, <br />I bet you gonna
+          love the stuff I'll be mailing you!
+          <br />I don't spam, really!
+        </p>
+        <p>
+          <input
+            type="email"
+            style={{ width: '140px' }}
+            name="email"
+            id="paemail"
+            placeholder="Your awesome email address!"
+            required
+          />
+        </p>
+        <input type="hidden" value="1" name="embed" />
+        <input type="submit" value="Welcome to the club 🔥" />
+      </form>
     </>
   )
 }
