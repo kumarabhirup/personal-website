941f4e3942eeedfe12e091579d0aaeb31d6d4908
libertyranny <126412059+libertyranny@users.noreply.github.com>
2023-03-28 00:49:15 +0530

    📦 NEW: About Korea Blockchain Week

--- packages/web/constants/Meta.js
+++ packages/web/constants/Meta.js
@@ -17,7 +17,22 @@ export const ELEMENTS = {
       projects. I am specialized in building full stack applications that use
       React in frontend and Node.js, GraphQL in the backend.
       <br />
-      <br />I am in the evergreen process of learning, and I am loving it!
+      <br />
+      Led a hackathon team in Korea Blockchain Week in August 2022, and won 2nd
+      place at two hackathons for{' '}
+      <a href="https://judiciary.app" target="_blank" rel="noopener noreferrer">
+        judiciary.app
+      </a>
+      , spent three months in{' '}
+      <a
+        href="https://nonce.community"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Nonce Korea hackerhouse
+      </a>{' '}
+      in Seoul, South Korea. I am in the evergreen process of learning, and I am
+      loving it!
       <br />
       <br />
       Formerly worked as a{' '}
@@ -40,7 +55,7 @@ export const ELEMENTS = {
 
 # 🏆 About me
 
-I am **Kumar Abhirup, a 19-year-old Software Developer**, founder of [Beam](https://itsbeam.com). Currently working as a Software Engineer at [Serendipity](https://serendipity.lol) <br /> To connect, you may DM me on [twitter](https://kumareth.com/twitter).
+I am **Kumar Abhirup, a 19-year-old Software Developer**, founder of [Beam](https://itsbeam.com). Currently working as a Software Engineer at a stealth startup. <br /> To connect, you may DM me on [twitter](https://kumareth.com/twitter).
 
 _Love what I write? [Subscribe to **my newsletter**](https://kumareth.com/subscribe) and stay updated about my content in general, lol._
   `,
